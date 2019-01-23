7952cd04ee5a445bf5de99c4ad30b759024dc980
jjc0803 <jcodemo@spp.org>
2019-01-22 21:42:51 -0600

    Adding visual elements

---
+++ XMLToolBench/Services/Models/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Services.Models
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+
+
+
+
+
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+}
--- XMLToolBench/XMLToolBench/MainWindow.xaml.cs
+++ XMLToolBench/XMLToolBench/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Deployment.Application;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace XMLToolBench
 {
@@ -23,6 +11,28 @@ namespace XMLToolBench
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MnuExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MnuAbout_OnClick(object sender, RoutedEventArgs e)
+        {
+            string version;
+            try
+            {
+                version = ApplicationDeployment.IsNetworkDeployed ?
+                    ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString() :
+                    System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+            catch (InvalidDeploymentException)
+            {
+                version = "(not installed)";
+            }
+            MessageBox.Show($"Checkouts - Current Version: {version}");
+
         }
     }
 }
